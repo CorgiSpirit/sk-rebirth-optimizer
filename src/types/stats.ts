a2bee1bf16d5eafe06ad0d784fb2387844a67d12
CorgiSpirit <goldcreeper@gmail.com>
2026-03-07 03:05:21 +0700

    scraping old setup, start whole new using node.js, npm, react+vite, tailwindcss, zustand

---
+++ src/types/stats.ts
@@ -0,0 +1,17 @@
+export interface Stats {
+  attack_f: number;
+  attack_p: number;
+  defense_f: number;
+  defense_p: number;
+  hp_f: number;
+  hp_p: number;
+  speed_f: number; 
+  crit_rate_p: number;
+  crit_damage_p: number;
+  weakness_hit_chance_p: number;
+  weakness_hit_damage_p: number;
+  block_rate_p: number;
+  damage_reduction_p: number;
+  effect_hit_p: number;
+  effect_res_p: number;
+}

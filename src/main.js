524d286dd14d94679d2e1ace7c82c6e205de9625
CorgiSpirit <goldcreeper@gmail.com>
2026-03-06 19:48:38 +0700

    initial page setup with static file

---
+++ src/main.js
@@ -0,0 +1,5 @@
+console.log("Script loaded successfully.");
+
+// This line finds the <p> tag with the id="status" 
+// and changes the text to confirm the JS is working.
+document.getElementById("status").innerText = "System Status: Online and Ready.";
